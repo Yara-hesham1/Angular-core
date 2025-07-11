f42ecf0a0130cc40fcd8b51d9bc2a4d0a5de426b
yara yora <yarahesham316@gmail.com>
2025-07-12 02:59:15 +0300

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from './course-card/course-card.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    CourseCardComponent
+  
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
